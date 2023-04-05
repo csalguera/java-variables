da8d3a04dd2cdef7269f5389ababd5d78d004aee
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-05 17:19:30 -0400

    create Char.java file and solve Char exercise

---
+++ src/Char.java
@@ -0,0 +1,10 @@
+public class Char {
+  public static void main(String[] args) {
+    // Create a variable called expectedGrade of type char.
+    char expectedGrade;
+    // Fill it with a single letter, representing the grade you think you would get in a graded Java course where the grades A, B, C, D and F are possible.
+    expectedGrade = 'A';
+    // Print out your expectedGrade variable!
+    System.out.println(expectedGrade);
+  }
+}
