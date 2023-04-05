208b407f5cb05f53f97e4f7495864a479d671bc2
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-05 16:44:48 -0400

    solve Creator Exercise

--- src/Creator.java
+++ src/Creator.java
@@ -1,9 +1,13 @@
 public class Creator {
   public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
     // In Creator.java, we have defined some variables related to James Gosling, the creator of Java.
 
     // Inside main(), use System.out.println() to print out the variable name.
-    String name = "James Gosling";
-    int yearCreated = 1995;
+    System.out.println(name);
+
+    // Use the same command to print out yearCreated.
+    System.out.println(yearCreated);
   }
 }
