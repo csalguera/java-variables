d93e308347f722e4b07dbc36de42c363edd693b0
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-05 16:42:35 -0400

    touch and stub up Creator.java file

---
+++ src/Creator.java
@@ -0,0 +1,9 @@
+public class Creator {
+  public static void main(String[] args) {
+    // In Creator.java, we have defined some variables related to James Gosling, the creator of Java.
+
+    // Inside main(), use System.out.println() to print out the variable name.
+    String name = "James Gosling";
+    int yearCreated = 1995;
+  }
+}
