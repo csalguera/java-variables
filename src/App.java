f141e836e4f9a137a22b2d461806d13926d5c9d5
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-05 16:39:52 -0400

    provide comments on basic principles of variables in Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Some primitive data types are: int, double, and boolean
+        // Structure:
+        // datatype variableName
+        int age;
+        double salaryRequirement;
+        boolean isEmployed;
+
+        // assignment operator
+        // assign value to a variable with the assignment operator '='
+        age = 85;
+        // when code is used to represent a fixed value, like 85, it is referred to as a literal
+
+        // assign value to to a decalred variable in one line:
+        int yearCodecademyWasFounded = 2011;
+
+        // Statement with concatenation:
+        System.out.println("Codecademy was founded in " + yearCodecademyWasFounded);
     }
 }
