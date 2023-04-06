6d1e68b8b29123d0d991a04223e71042d99e3e74
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-06 16:01:57 -0400

    solve Mess exercise

--- src/Mess.java
+++ src/Mess.java
@@ -1,9 +1,9 @@
 public class Mess {
 	public static void main(String[] args) {   
-		String year = 2001;
-    double title = "Shrek";
-    int genre = 'C';
-    boolean runtime = 1.58;
-    char isPG = true;
+		int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
 	}
 }
