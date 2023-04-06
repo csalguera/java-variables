c6e78b657f423dc940141dde30a03c0ace58362f
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-06 16:29:31 -0400

    finish MyProfile exercise

--- src/MyProfile.java
+++ src/MyProfile.java
@@ -2,6 +2,13 @@ public class MyProfile {
   public static void main(String[] args) {
     // The file MyProfile.java contains a class that represents your hiring profile as presented to potential employers.
     // In the main() method, create a variable called name that holds your name, as a sequence of characters.
-    
+    String name = "Carlos";
+    // Create a variable called age that holds your age as a whole number.
+    int age = 26;
+    // Create a variable called desiredSalary that holds your desired salary per year to a precision of two decimal points.
+    double desiredSalary = 70000.00;
+    // Create a variable called gender that holds a single character, m (male), f (female), n (for none), or o (for other).
+    char gender = 'm';
+    boolean lookingForJob = true;
   }
 }
